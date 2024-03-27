3e806fc78ff97514e639fca74bfa2213ead49fcc
Anandharaju03 <165182753+Anandharaju03@users.noreply.github.com>
2024-03-27 22:43:21 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+
+	 
+     function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}let slideIndex = 0;
+showSlides();
+
---
+++ js2.js
@@ -0,0 +1,28 @@
+ let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 9000); // Change image every 2 seconds
+}
+
+// Function to move slides forward or backward
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+{
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}let slideIndex = 0;
+showSlides();
+}
